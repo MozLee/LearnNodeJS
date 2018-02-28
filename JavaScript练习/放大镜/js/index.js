142f23c7aaf368a2e66195340f3eff7f0108a15f
MozLee <lixin@mozlee.com>
2018-02-28 16:39:59 +0800

    格式化代码

--- JavaScript练习/放大镜/js/index.js
+++ JavaScript练习/放大镜/js/index.js
@@ -4,9 +4,9 @@ const mask = document.querySelector('#mask');
 const rightImg = document.querySelector('#right img');
 const rightBorder = document.querySelector('#border')
 //处理比例
-rightImg.style.width = rightBorder.clientWidth/(parseInt(getComputedStyle(mask).width)/leftArea.clientWidth) +'px';
-rightImg.style.height = rightBorder.clientHeight/(parseInt(getComputedStyle(mask).height)/leftArea.clientHeight) + 'px';
-console.log(rightBorder.clientWidth,parseInt(getComputedStyle(mask).height),leftArea.clientWidth)
+rightImg.style.width = rightBorder.clientWidth / (parseInt(getComputedStyle(mask).width) / leftArea.clientWidth) + 'px';
+rightImg.style.height = rightBorder.clientHeight / (parseInt(getComputedStyle(mask).height) / leftArea.clientHeight) + 'px';
+console.log(rightBorder.clientWidth, parseInt(getComputedStyle(mask).height), leftArea.clientWidth)
 //mask移动
 leftArea.onmousemove = (e) => {
     //定位缩略图区
@@ -17,27 +17,27 @@ leftArea.onmousemove = (e) => {
         mask.clientHeight / 2;
 
     // 处理边界
-    if(disX<0){
-        disX=0;
+    if (disX < 0) {
+        disX = 0;
     }
-    if(disY<0){
-        disY=0;
+    if (disY < 0) {
+        disY = 0;
     }
-    if(disX>leftArea.clientWidth-mask.clientWidth){
-        disX = leftArea.clientWidth-mask.clientWidth;
+    if (disX > leftArea.clientWidth - mask.clientWidth) {
+        disX = leftArea.clientWidth - mask.clientWidth;
     }
-    if(disY>leftArea.clientHeight-mask.clientHeight){
-        disY = leftArea.clientHeight-mask.clientHeight;
+    if (disY > leftArea.clientHeight - mask.clientHeight) {
+        disY = leftArea.clientHeight - mask.clientHeight;
     }
     mask.style.left = disX + 'px';
     mask.style.top = disY + 'px';
-    
+
     //计算比例
-    let scaleX = disX/leftArea.clientWidth;
-    let scaleY = disY/leftArea.clientHeight;
+    let scaleX = disX / leftArea.clientWidth;
+    let scaleY = disY / leftArea.clientHeight;
     //定位放大区
-    rightImg.style.left = -rightImg.clientWidth*scaleX +'px';
-    rightImg.style.top = -rightImg.clientHeight*scaleY +'px';
+    rightImg.style.left = -rightImg.clientWidth * scaleX + 'px';
+    rightImg.style.top = -rightImg.clientHeight * scaleY + 'px';
 }
 
 leftArea.onmouseout = () => {
