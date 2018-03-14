d5520e1450c422a3fe5b6cb36ee54b8e1c9712c8
MozLee <lixin@mozlee.com>
2018-03-14 15:59:19 +0800

    err test

--- testblog/app.js
+++ testblog/app.js
@@ -1,10 +1,18 @@
 //引入express
 const express = require('express');
+const swig = require('swig')
 //创建app
 const app = express();
 //设置模板
-app.engine('html',swig.renderFile);
 app.set('views','./views');
-app.set('views engine','html');
+app.set('view engine','html');
+app.engine('.html', swig.renderFile);
+//取消模板缓存限制
+swig.setDefaults({cache:false});
+//路由
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
 
 app.listen(3000);
