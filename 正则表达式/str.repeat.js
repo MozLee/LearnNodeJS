006bc764c70feec6110d5f284e731be64d651c26
MozLee <lixin@mozlee.com>
2018-03-19 15:45:10 +0800

    email

--- 正则表达式/str.repeat.js
+++ 正则表达式/str.repeat.js
@@ -7,3 +7,10 @@ let re = /w+/g;
 let newStr = str.replace(re, item => '*'.repeat(item.length));
 
 console.log(newStr);
+let re2 = /[_0-9a-zA-Z]+@[0-9a-zA-Z]+(\.[a-z]{2,5}){1,3}/g
+let re3 = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/g
+console.log(re2.test('68879747@qq.com1211113'));
+
+let re4 = /123/;
+let str2 = '1234'
+console.log(re4.test('1234'));
